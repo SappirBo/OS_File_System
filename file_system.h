06fc831717e6943eff9f1098fea2a208a5a5a33a
sappir <sappirbohbot@gmail.com>
2022-06-15 22:31:18 +0300

    adding to  h

--- file_system.h
+++ file_system.h
@@ -191,7 +191,18 @@ int myclosedir(myDIR *dirp);
 
 
 
+///////////////////////////////////////////////////////////////
+//  Last Part Laft to do.
+////////////////////////////////////////////////////////////////
+struct myFILE{}myFILE;
 
+myFILE *myfopen(const char *restrict pathname, const char *restrict mode);
+int myfclose(myFILE *stream);
+size_t myfread(void *restrict ptr, size_t size, size_t nmemb,myFILE *restrict stream);
+size_t myfwrite(const void *restrict ptr, size_t size, size_tnmemb, myFILE *restrict stream);
+int myfseek(myFILE *stream, long offset, int whence);
+int myfscanf(myFILE *restrict stream,const char *restrict format, ...);
+int myfprintf(myFILE *restrict stream,const char *restrict format, ...);
 
 
 
